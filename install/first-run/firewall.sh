1d23b7da7d73b9b9ea217408face82009348b299
nytr0gen <6614470+nytr0gen@users.noreply.github.com>
2026-08-13 18:33:53 +0300

    fix screenrecord

--- install/first-run/firewall.sh
+++ install/first-run/firewall.sh
@@ -11,6 +11,7 @@ sudo ufw allow 22/tcp
 
 # Allow Docker containers to use DNS on host
 sudo ufw allow in proto udp from 172.16.0.0/12 to 172.17.0.1 port 53 comment 'allow-docker-dns'
+sudo ufw allow in proto udp from 192.168.0.0/16 to 172.17.0.1 port 53 comment 'allow-docker-dns'
 
 # Turn on the firewall
 sudo ufw --force enable
